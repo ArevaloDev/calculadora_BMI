97a186450343708d22d26ce1a88e72bd36c37ab3
ArevaloDev <arevalodaniel999@gmail.com>
2025-03-06 13:38:43 -0600

    agregando estilos a inicio componente y tambien agregango funciones para aumentar edad y peso tambien altura

--- calculadora/src/app/components/inicio/inicio.component.ts
+++ calculadora/src/app/components/inicio/inicio.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class InicioComponent {
 
+  public edad:number = 18;
+  public peso:number = 130;
+  public altura:number = 140;
+
+
+
+  actualizarEdad = (option:string):void => {
+    if((option === 'plus' && this.edad < 100) || (option === 'minus' && this.edad > 0 ) ){
+      this.edad = option == 'plus' ? this.edad + 1 : this.edad - 1;
+
+    }
+  }
+
+  actualizarPeso = (option:string):void => {
+    if(option === 'minus' && this.peso <= 0) return;
+      this.peso = option == 'plus' ? this.peso + 1 : this.peso - 1;
+
+  }
+
+  actualizarAltura = (event:number):void => {
+    console.log(event);
+
+  }
+
 }
